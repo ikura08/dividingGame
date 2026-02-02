d9794ddf4c4d59f8195eca1b74447751b7b38868
ikuraHt <haruki.togawa0814@gmail.com>
2026-02-02 13:18:20 +0900

    セレクト画面整理

--- Assets/Scripts/SelectStage.cs
+++ Assets/Scripts/SelectStage.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Timeline;
 
 public class SelectStage : MonoBehaviour
 {
     public GameObject[] clearStageBack;
     public GameObject[] movingLight;
     public Button[] buttons;
-    public static int nextStage =1;
+    public GameObject[] signs;
+    public GameObject[] newSigns;
+    public static int nextStage =2;
     public static int clearCount = nextStage -1;
     // Start is called before the first frame update
     void Start()
@@ -28,6 +31,14 @@ public class SelectStage : MonoBehaviour
         {
             obj.gameObject.SetActive(false);
         }
+        foreach (GameObject obj in signs)
+        {
+            obj.SetActive(false);
+        }
+        foreach (GameObject obj in newSigns)
+        {
+            obj.SetActive(false);
+        }
 
         // クリア数分だけ表示
         for (int i = 0; i <= clearCount && i < clearStageBack.Length; i++)
@@ -40,8 +51,19 @@ public class SelectStage : MonoBehaviour
             // buttons[i].onClick.AddListener(() => LoadStage(stageIndex));
         }
 
-        if (movingLight[nextStage-1] != null)
+        for (int i = 0; i <= clearCount - 1 && i < clearStageBack.Length; i++)
+        {
+            signs[i].SetActive(true);
+
+            int stageIndex = i + 1;
+            // buttons[i].onClick.AddListener(() => LoadStage(stageIndex));
+        }
+
+        if (movingLight[nextStage - 1] != null)
+        {
             movingLight[nextStage - 1].SetActive(true);
+            newSigns[nextStage - 1].SetActive(true);
+        }
     }
 
     // Update is called once per frame
